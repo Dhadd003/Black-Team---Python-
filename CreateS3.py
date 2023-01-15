1589fbd4cacc2268fab4da68132a4e8f14b02f89
Dhadd003 <117035008+Dhadd003@users.noreply.github.com>
2023-01-15 15:15:13 -0500

    Update CreateS3.py

--- CreateS3.py
+++ CreateS3.py
@@ -1,10 +1,13 @@
 import boto3
-aws_resource = boto3.resource('s3')
-bucket = aws_resource.Bucket('luit2023p')
-response = bucket.create(
+
+#Create S3
+
+aws_resource=boto3.resource('s3')
+bucket=aws_resource.Bucket('luit2023p')
+response=bucket.create(
     ACL='public-read',
     CreateBucketConfiguration={
-        'LocationConstraint': 'us-east-2'
+        'LocationConstraint':'us-east-2'
     }
 )
 
