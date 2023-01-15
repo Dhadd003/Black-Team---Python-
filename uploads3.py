94f2cb18541828c2e0396e8501326a176b729a1b
Dhadd003 <117035008+Dhadd003@users.noreply.github.com>
2023-01-15 15:14:28 -0500

    Update uploads3.py

--- uploads3.py
+++ uploads3.py
@@ -1,6 +1,6 @@
 import boto3
 
-#upload single file
+#How to Upload Single File
 
 s3_resource=boto3.client(s3)
 s3_resource.upload_file(
@@ -8,19 +8,19 @@ s3_resource.upload_file(
     Bucket='mybucket-boto3',
     Key='test.txt')
 
-#upload multiple files
+#How to Upload Multiple Files
 
 import os
 import glob
 
-cwd = os.cwd()
-cwd = cwd + '/upload/'
-files = glob.glob(cwd + '*.png')
+cwd=os.cwd()
+cwd=cwd + '/upload/'
+files=glob.glob(cwd + '*.png')
 print(files)
 
-#for file in files
+#For File in Files
 
-s3_resource = boto3.resource('s3')
+s3_resource=boto3.resource('s3')
 s3_resource.upload_file(
 Filename='test',
 Bucket='mybucket-boto3',
