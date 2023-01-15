110911b53555f8187750f1b0f8ab7e6b62c75841
Dhadd003 <117035008+Dhadd003@users.noreply.github.com>
2023-01-15 15:48:32 -0500

    Create TerminateMultipleEC2s.py

---
+++ TerminateMultipleEC2s.py
@@ -0,0 +1,16 @@
+import boto3
+
+#TerminateMultipleEC2s
+
+ec2_client=boto3.client("ec2")
+x=ec2_client.describe_instances()
+data=x["Reservations"]
+li=[]
+for instances in data:
+    instance=instances["Instances"]
+    for ids in instance:
+        instance_id=ids["InstanceId"]
+        li.append(instance_id)
+        
+    
+ec2_client.terminate_instances(InstanceIds=li)
